ae1353746e7c73dd80202a611027322c2bbb5325
GabrielAlbSil <gabrielalbuquerquemoura@gmail.com>
2021-11-16 19:21:08 -0300

    primeria commit

---
+++ ExercicioTelas/src/main/Main.java
@@ -0,0 +1,14 @@
+
+package main;
+
+import View.cadastro_view;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        new cadastro_view().setVisible(true);
+    }
+    
+}
